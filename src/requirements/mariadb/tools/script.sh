f222c0f3afde348d6eb0463aa616af2dd6b63f55
El Mehdi Kenane <elmehdi.kenane@gmail.com>
2024-05-06 15:10:32 +0100

    stable version

--- src/requirements/mariadb/tools/script.sh
+++ src/requirements/mariadb/tools/script.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-service mysql start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+service mariadb start
 
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysqld
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+sleep 5
+
+mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
+
+mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
+
+# next line should be modifie to give privileges only to the maria database
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';"
 
 mysql -e "FLUSH PRIVILEGES;"
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
 
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
--- src/requirements/wordpress/conf/auto_config.sh
+++ src/requirements/wordpress/conf/auto_config.sh
@@ -2,9 +2,27 @@
 # to make sure the MariaDB database has had time to launch correctly.
 sleep 10
 
-if [ ! -f "wp-config.php" ]; then
-    wp config create --allow-root --path=/var/www/html
-    # search about "--allow-root"
-else
-    echo "wp-config.php already exists. Skipping configuration."
-fi
+cd /var/www/html
+wp core download --allow-root
+
+# replace sed command with wp config set 
+sed -i '23 s/database_name_here/maria/' /var/www/html/wp-config-sample.php
+sed -i '26 s/username_here/aa/' /var/www/html/wp-config-sample.php
+sed -i '29 s/password_here/aa/' /var/www/html/wp-config-sample.php
+sed -i '32 s/localhost/maria:3306/' /var/www/html/wp-config-sample.php
+sed -i '82 s/false/true/' /var/www/html/wp-config-sample.php
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+# Create directory for sock-file
+mkdir -p /run/php
+
+wp core install --url="localhost" --title="My Wordpress Site" --admin_user="ekenane" --admin_password="123" --admin_email="appdevelopperpro@gmail.com" --allow-root
+
+# if [ ! -f "wp-config.php" ]; then
+#     # search about "--allow-root"
+#     wp config create --allow-root --path=/var/www/html --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306
+# else
+#     echo "wp-config.php already exists. Skipping configuration."
+# fi
+
+/usr/sbin/php-fpm7.4 -F
+# /usr/sbin/php-fpm7.4
