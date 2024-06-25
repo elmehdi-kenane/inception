545153f188104cb0f0a7cc1a3f4007806cf93d5a
El Mehdi Kenane <elmehdi.kenane@gmail.com>
2024-06-25 16:27:32 +0100

    used env variables in the scripts - removed comments

--- src/requirements/mariadb/tools/script.sh
+++ src/requirements/mariadb/tools/script.sh
@@ -1,52 +1,19 @@
 #!/bin/bash
 
-# echo "============== mysqld_safe & output =============="
 mysqld_safe &
-# echo "============== ==================== =============="
-# service mariadb start
 
 sleep 5
 
-if [ -d "/var/lib/mysql/maria" ]
-then 
-	echo "++++++++++++++++++++ maria Directory exists ++++++++++++++++++++"
-else
-	echo "-------------------- maria Directory does not exists --------------------"
-fi
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE_NAME};"
 
-if [ -d "/var/lib/mysql" ]
-then 
-	echo "++++++++++++++++++++ mysql Directory exists ++++++++++++++++++++"
-else
-	echo "-------------------- mmysql Directory does not exists --------------------"
-fi
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USERNAME}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-if [ -d "/var/lib/mysql/mysql" ]
-then 
-	echo "++++++++++++++++++++ mysql/mysql Directory exists ++++++++++++++++++++"
-else
-	echo "-------------------- mysql/mysql Directory does not exists --------------------"
-    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-    sleep 5
-fi
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE_NAME}.* TO '${MYSQL_USERNAME}'@'%';"
 
-until mysqladmin ping --silent; do
-    echo "============== Waiting for MariaDB to start... =============="
-    sleep 1
-done
-
-echo "============== MariaDB is up and running. =============="
-
-mysql -e "CREATE DATABASE IF NOT EXISTS maria;"
-
-mysql -e "CREATE USER IF NOT EXISTS 'aa'@'%' IDENTIFIED BY 'aa';"
-
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'aa'@'%';"
-
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
 mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p"123" shutdown
+mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 
 exec mysqld_safe
--- src/requirements/wordpress/conf/auto_config.sh
+++ src/requirements/wordpress/conf/auto_config.sh
@@ -4,34 +4,20 @@ sleep 10
 
 cd /var/www/html
 wp core download --allow-root
-# rm -rf /var/www/html/*
 
 if [ ! -f "wp-config.php" ]; then
     mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 fi
 
-wp config set DB_NAME maria --allow-root --path='/var/www/html'
-wp config set DB_USER aa --allow-root --path='/var/www/html'
-wp config set DB_PASSWORD aa --allow-root --path='/var/www/html'
-wp config set DB_HOST mariadb:3306 --allow-root --path='/var/www/html'
-# wp config set WP_DEBUG true --allow-root --path='/var/www/html'
-
-# sed -i '23 s/database_name_here/maria/' /var/www/html/wp-config.php
-# sed -i '26 s/username_here/aa/' /var/www/html/wp-config.php
-# sed -i '29 s/password_here/aa/' /var/www/html/wp-config.php
-# sed -i '32 s/localhost/maria:3306/' /var/www/html/wp-config.php
-# sed -i '82 s/false/true/' /var/www/html/wp-config.php
+wp config set DB_NAME $MYSQL_DATABASE_NAME --allow-root --path=$WP_DIR
+wp config set DB_USER $MYSQL_USERNAME --allow-root --path=$WP_DIR
+wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=$WP_DIR
+wp config set DB_HOST $WP_DB_HOST --allow-root --path=$WP_DIR
+# wp config set WP_DEBUG true --allow-root --path=$WP_DIR
 
 # Create directory for sock-file
 mkdir -p /run/php
 
-wp core install --url="localhost" --title="My Wordpress Site" --admin_user="ekenane" --admin_password="123" --admin_email="appdevelopperpro@gmail.com" --allow-root
-
-# if [ ! -f "wp-config.php" ]; then
-#     # search about "--allow-root"
-#     wp config create --allow-root --path=/var/www/html --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306
-# else
-#     echo "wp-config.php already exists. Skipping configuration."
-# fi
+wp core install --url=${WP_URL} --title="${WP_TITLE}" --admin_user=${WP_ADMIN_USERNAME} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
 
 /usr/sbin/php-fpm7.4 -F
