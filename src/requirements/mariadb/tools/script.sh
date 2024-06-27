fb1b4217601871496f85c4b5e08812598ba7a16c
El Mehdi Kenane <elmehdi.kenane@gmail.com>
2024-06-27 04:07:36 +0100

    Restored MariaDB wait loop in startup script

--- src/requirements/mariadb/tools/script.sh
+++ src/requirements/mariadb/tools/script.sh
@@ -2,7 +2,12 @@
 
 mysqld_safe &
 
-sleep 5
+until mysqladmin ping --silent; do
+    echo "          Waiting for MariaDB to start..."
+    sleep 1
+done
+
+echo "============== MariaDB is up and running. =============="
 
 mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE_NAME};"
 
@@ -12,8 +17,8 @@ mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE_NAME}.* TO '${MYSQL_USERNAME}
 
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
-mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
 
 exec mysqld_safe
